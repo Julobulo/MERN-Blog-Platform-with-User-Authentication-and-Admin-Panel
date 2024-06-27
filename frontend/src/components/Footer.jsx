559732f13c86609daa921749fd030e9d05845692
JR-Koders <jules.caoeiros@gmail.com>
2024-06-27 19:16:18 +0200

    Footer - change latest articles to actually work
    The design for the "latest articles" section is now available. It simply contains a list of links to the different latest articles, with hr between them

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,6 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
+const domainName = import.meta.env.VITE_API_BASE_URL;
 
 const Footer = () => {
+  const [latestPosts, setLatestPosts] = useState([]);
+  useEffect(() => {
+    axios.get(`${domainName}/latest`)
+      .then((response) => {
+        setLatestPosts(response.data)
+      })
+      .catch((error) => {
+        toast.error(error);
+        console.error(error);
+        setLatestPosts([
+          {
+            title: "example post 1"
+          },
+          {
+            title: "example post 2"
+          },
+          {
+            title: "example post 3"
+          },
+        ])
+      })
+  }, [])
   return (
     <footer className="bg-gray-900 py-6">
       <div className="container mx-auto px-4 flex flex-col justify-between">
@@ -15,21 +40,17 @@ const Footer = () => {
             <div className="mb-4 sm:mb-0">
               <h2 className="text-lg font-semibold mb-2 text-gray-200">Latest Posts</h2>
               <ul>
-                <li>
-                  <a href="#" className="text-gray-400 hover:text-gray-200">
-                    Post 1
-                  </a>
-                </li>
-                <li>
-                  <a href="#" className="text-gray-400 hover:text-gray-200">
-                    Post 2
-                  </a>
-                </li>
-                <li>
-                  <a href="#" className="text-gray-400 hover:text-gray-200">
-                    Post 3
-                  </a>
-                </li>
+              {latestPosts.map((post, index) => (
+                <div className="my-2">
+                  <li key={index} className="my-2">
+                    <a href={`/blog/article/${post.title}`} className="text-gray-400 hover:text-gray-200">{post.title}</a>
+                  </li>
+                  {
+                    (index < latestPosts.length - 1) ?
+                    (<hr className="my-2" />) : (<></>)
+                   }
+                </div>
+              ))}
               </ul>
             </div>
             <div>
