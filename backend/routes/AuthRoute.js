2e352587a716a4b03ebe009aab54f4445f9110f5
JR-Koders <jules.caoeiros@gmail.com>
2024-07-13 13:16:28 +0200

    Initialized OAuth Route files and imports
    The files and imports for oauth are now ready, I now need to define the routes that will handle oauth

--- backend/routes/AuthRoute.js
+++ backend/routes/AuthRoute.js
@@ -1,11 +1,13 @@
 import express from "express";
 import { Signup, Login } from "../Controllers/AuthController.js";
+import OAuth from "./OAuth.js";
 import userVerification from "../Middlewares/AuthMiddleware.js";
 
 const router = express.Router();
 
 router.post('/signup', Signup);
 router.post('/login', Login);
+router.use('/oauth', OAuth);
 router.post('/', userVerification);
 
 export default router
---
+++ backend/routes/OAuth.js
@@ -0,0 +1,7 @@
+import express from "express";
+
+const router = express.Router();
+
+// define oauth routes here
+
+export default router
