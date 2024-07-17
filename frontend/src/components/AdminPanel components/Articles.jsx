df0b776ceca39b3f765d7b3e0ebb7b30b252af02
JR-Koders <jules.caoeiros@gmail.com>
2024-07-17 23:39:40 +0200

    Added load more functionality (from Users.jsx)
    I used the same system as the one I created for Users.jsx

--- frontend/src/components/AdminPanel components/Articles.jsx
+++ frontend/src/components/AdminPanel components/Articles.jsx
@@ -4,33 +4,8 @@ import ArticleCard from '../ArticleCard';
 import { MdDeleteOutline } from "react-icons/md";
 import { FiEdit2 } from "react-icons/fi";
 import Cookies from "js-cookie";
-
-// Sample data for testing
-const posts = [
-    {
-        _id: 1,
-        title: "The Rise of Artificial Intelligence in Modern Society",
-        subtitle: "Explore how AI is transforming the healthcare industry.",
-        author: "John Doe",
-        tags: ["AI", "Healthcare"],
-        likes: 120,
-        imgSrc: "https://via.placeholder.com/400x200",
-        date: "2023-06-01",
-        description: "This article explores the rise of AI in modern society, focusing on its impact on the healthcare industry."
-    },
-    {
-        _id: 2,
-        title: "AI and Financial Services",
-        subtitle: "Discover the ways AI is revolutionizing the financial sector.",
-        author: "Jane Smith",
-        tags: ["AI", "Finance"],
-        likes: 98,
-        imgSrc: "https://via.placeholder.com/400x200",
-        date: "2023-06-02",
-        description: "This article discusses how AI is transforming the financial sector, improving efficiency and reducing costs."
-    },
-    // Add more posts as needed...
-];
+import axios from 'axios';
+import Spinner from '../Spinner';
 
 const Articles = () => {
     const navigate = useNavigate();
@@ -40,14 +15,59 @@ const Articles = () => {
         }
     }, []);
     const [searchQuery, setSearchQuery] = useState("");
+    const [articles, setArticles] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [loadingMore, setLoadingMore] = useState(false);
+    const [hasMore, setHasMore] = useState(true);
+    const [skip, setSkip] = useState(0);
+    const [wasHighlighted, setWasHighlighted] = useState(false);
     const location = useLocation();
+    const limit = 2;
 
-    const filteredPosts = posts.filter(post =>
-        post.title.toLowerCase().includes(searchQuery.toLowerCase()) ||
-        post.subtitle.toLowerCase().includes(searchQuery.toLowerCase()) ||
-        post.author.toLowerCase().includes(searchQuery.toLowerCase()) ||
-        post.tags.some(tag => tag.toLowerCase().includes(searchQuery.toLowerCase()))
-    );
+    const fetchArticles = (skip, search) => {
+        if (skip > 0) { setLoadingMore(true) } else { setLoading(true); setArticles([]) }
+        axios.get(
+            `http://localhost:5555/blog/adminpanel?skip=${skip}&search=${search}`,
+            { withCredentials: true }
+        )
+            .then((response) => {
+                if (response.data.length > 0) {
+                    setSkip(skip + limit)
+                    if (skip < 1) { // if skip < 1 then we have to erase everything in the array before
+                        setArticles([])
+                    }
+                    setArticles(articles => (articles ? articles.concat(response.data) : response.data));
+                    if (response.data.length < limit) {
+                        setHasMore(false);
+                    }
+                } else {
+                    setHasMore(false);
+                }
+                setLoading(false);
+                setLoadingMore(false);
+                setWasHighlighted(false);
+            })
+            .catch((error) => {
+                setLoading(false);
+                setLoadingMore(false);
+                console.log(error);
+                // Sample data for testing
+                setArticles([{
+                    imgSrc: 'https://via.placeholder.com/64',
+                    username: 'username',
+                    date: 'January 1, 2000',
+                    bio: 'bio',
+                    email: 'email',
+                    isAdmin: false,
+                }]);
+                // toast.error(`Couldn't fetch the data... Error: ${error.response.data.message}`);
+            })
+    }
+
+    useEffect(() => {
+        fetchArticles(skip, '')
+    }, []);
+
     return (
         <div className='flex-grow bg-black text-white'>
             <div className='container mx-auto px-4 py-8'>
@@ -70,31 +90,47 @@ const Articles = () => {
                         className="mb-4 px-4 py-2 rounded-lg w-full text-black dark:text-white"
                     />
                     <div className=" w-full">
-                        {filteredPosts.map(post => (
+                        {loading ? (
+                            <div className='mb-5'>
+                                <Spinner />
+                            </div>
+                        ) : (articles.map(article => (
                             <div className='flex flex-row'>
                                 <div className='basis-11/12'>
                                     <ArticleCard
-                                        key={post._id}
-                                        title={post.title}
-                                        description={post.description}
-                                        href={`/blog/article/${post._id}`}
-                                        author={post.author}
-                                        date={post.date}
-                                        tags={post.tags}
-                                        imgSrc={post.imgSrc}
-                                        hearts={post.likes}
+                                        key={article._id}
+                                        title={article.title}
+                                        subtitle={article.subtitle}
+                                        href={`/blog/article/${article.title}`}
+                                        author={article.author}
+                                        date={article.date}
+                                        tags={article.tags}
+                                        imgSrc={article.image}
+                                        likes={article.likes}
                                     />
                                 </div>
                                 <div className="basis-1/12 flex flex-col justify-evenly items-center">
-                                    <Link to={`/AdminPanel/Articles/Edit/${post._id}`} className='text-yellow-400 '>
+                                    <Link to={`/AdminPanel/Articles/Edit/${article._id}`} className='text-yellow-400 '>
                                         <FiEdit2 />
                                     </Link>
-                                    <Link to={`/AdminPanel/Articles/Delete/${post._id}`} className='text-red-400'>
+                                    <Link to={`/AdminPanel/Articles/Delete/${article._id}`} className='text-red-400'>
                                         <MdDeleteOutline />
                                     </Link>
                                 </div>
-                            </div>
+                            </div>)
                         ))}
+                        {loadingMore ? (<Spinner />) : (
+                            hasMore && (
+                                <button
+                                    onClick={() => fetchArticles(skip, searchQuery)}
+                                    className="w-full bg-green-600 text-white py-2 px-4 rounded-md shadow-md hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
+                                >
+                                    Load More
+                                </button>
+                            )
+                        )
+                        }
+                        {!hasMore && <p className='text-center'>No more articles to load</p>}
                     </div>
                 </div>
             </div>
