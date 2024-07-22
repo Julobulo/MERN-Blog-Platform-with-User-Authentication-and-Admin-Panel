7064934b3cac3def2e5c19039e31a68990efbb47
JR-Koders <jules.caoeiros@gmail.com>
2024-07-22 12:27:50 +0200

    CreateForm.jsx cleanup
    Cleaned up all unused code, ...

--- frontend/src/components/CreateForm.jsx
+++ frontend/src/components/CreateForm.jsx
@@ -16,17 +16,15 @@ import "./CreateForm.css";
 
 // TODO: put all changes in localStorage
 
-const CreateForm = ({ pageTitle, defaultImage, defaultTitle, defaultSubtitle, defaultTags, defaultMainContent }) => {
+const CreateForm = ({ pageTitle, defaultImage, defaultTags, defaultMainContent }) => {
     const navigate = useNavigate();
     useEffect(() => {
         if (!Cookies.get('token')) {
             navigate('/login')
         }}, [navigate])
     const [image, setImage] = useState();
-    // const [title, setTitle] = useState('');
-    // const [subtitle, setSubtitle] = useState('');
     const [tags, setTags] = useState([]);
-    // const [mainContent, setMainContent] = useState('');
+    const [blocks, setBlocks] = useState([]);
     const [tagInput, setTagInput] = useState('');
 
     const handleImageChange = async (event) => {
@@ -123,112 +121,6 @@ const CreateForm = ({ pageTitle, defaultImage, defaultTitle, defaultSubtitle, de
         }
     };
 
-    // const toolbarOptions = [
-    //     [{ header: [1, 2, 3, false] }],
-    //     ["bold", "italic", "underline", "strike",
-    //         // "blockquote"
-    //     ],
-    //     // [{ list: "ordered" }, { list: "bullet" }],
-    //     ["link"],
-    //     [{ color: [] }],
-    //     // ["code-block"],
-    //     ["clean"],
-    // ];
-
-    // useEffect(() => {
-    //     const quill = new Quill('#editor', {
-    //         modules: {
-    //             toolbar: {
-    //                 container: toolbarOptions,
-    //             },
-    //         },
-    //         theme: 'snow',
-    //     });
-
-    //     quill.on('text-change', () => {
-    //         setMainContent(quill.root.innerHTML);
-    //     });
-
-    //     return () => {
-    //         quill.off('text-change');
-    //         // quill.dispose();
-    //     };
-    // }, []);
-
-    // return (
-    //     <div className="bg-black p-6 text-green-400">
-    //         <div className="my-5 max-w-3xl mx-auto p-6 bg-gray-900 rounded-lg shadow-md">
-    //             <h1 className="text-2xl font-bold mb-6">{pageTitle} Post</h1>
-    //             <div className="mb-4">
-    //                 <label htmlFor="image" className="block text-sm font-medium mb-1">Image (it will appear on the article display)</label>
-    //                 <input
-    //                     id="profilePicture"
-    //                     type="file"
-    //                     accept="image/*"
-    //                     onChange={handleImageChange}
-    //                     className="w-full px-3 py-2 border border-gray-700 rounded-md bg-gray-800 text-gray-400 focus:outline-none focus:ring-green-500 focus:border-green-500"
-    //                 />
-    //             </div>
-    //             <div className="mb-4">
-    //                 <label htmlFor="title" className="block text-sm font-medium mb-1">Title</label>
-    //                 <input
-    //                     type="text"
-    //                     id="title"
-    //                     value={title}
-    //                     minLength={10}
-    //                     maxLength={100}
-    //                     onChange={(e) => setTitle(e.target.value)}
-    //                     placeholder={`The Future of Quantum Computing`}
-    //                     className="w-full px-3 py-2 border border-gray-700 rounded-md bg-gray-800 text-green-400 focus:outline-none focus:ring-green-500 focus:border-green-500"
-    //                 />
-    //             </div>
-    //             <div className="mb-4">
-    //                 <label htmlFor="subtitle" className="block text-sm font-medium mb-1">Subtitle</label>
-    //                 <input
-    //                     type="text"
-    //                     id="subtitle"
-    //                     value={subtitle}
-    //                     minLength={30}
-    //                     maxLength={150}
-    //                     onChange={(e) => setSubtitle(e.target.value)}
-    //                     placeholder={`Curious about the future of Quantum Computing? Check out this article!`}
-    //                     className="w-full px-3 py-2 border border-gray-700 rounded-md bg-gray-800 text-green-400 focus:outline-none focus:ring-green-500 focus:border-green-500"
-    //                 />
-    //             </div>
-    //             <div className="mb-4">
-    //                 <label htmlFor="tags" className="block text-sm font-medium mb-1">Tags (up to 4)</label>
-    //                 <input
-    //                     type="text"
-    //                     id="tags"
-    //                     value={tagInput}
-    //                     onChange={(e) => setTagInput(e.target.value)}
-    //                     onKeyDown={handleTagInputKeyDown}
-    //                     placeholder={`Press Enter or comma to add tag (ex: "Quantum Computing", "Science", "Future", ...)`}
-    //                     className="w-full px-3 py-2 border border-gray-700 rounded-md bg-gray-800 text-green-400 focus:outline-none focus:ring-green-500 focus:border-green-500"
-    //                 />
-    //                 <div className="mt-2 flex flex-wrap">
-    //                     {tags.map((tag, index) => (
-    //                         <span key={index} className="bg-gray-800 border border-gray-700 rounded-full px-3 py-1 text-xs mr-2 mt-2">
-    //                             {tag} <button type="button" onClick={() => handleTagRemove(index)} className="ml-1 text-red-500">×</button>
-    //                         </span>
-    //                     ))}
-    //                 </div>
-    //             </div>
-    //             <div className="mb-4 text-white">
-    //                 <label htmlFor="mainContent" className="text-green-400 block text-sm font-medium mb-1">Content</label>
-    //                 <div id="editor" style={{ height: "300px" }}></div>
-    //             </div>
-    //             <button
-    //                 onClick={handlePost}
-    //                 className="w-full bg-green-500 text-white py-2 px-4 rounded-md shadow-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
-    //             >
-    //                 {pageTitle === 'Create' ? 'Create' : 'Save'}
-    //             </button>
-    //         </div>
-    //     </div>
-    // );
-    // Stores the document JSON.
-    const [blocks, setBlocks] = useState([]);
     // Creates a new editor instance.
     const editor = useCreateBlockNote({
         initialContent: [
@@ -837,7 +729,6 @@ const CreateForm = ({ pageTitle, defaultImage, defaultTitle, defaultSubtitle, de
         ],
     });
 
-    // Renders the editor instance using a React component.
     return (
         <div className="flex-grow bg-black p-6 text-green-400">
             <div className="my-5 max-w-3xl mx-auto p-6 bg-gray-900 rounded-lg shadow-md">
@@ -858,8 +749,7 @@ const CreateForm = ({ pageTitle, defaultImage, defaultTitle, defaultSubtitle, de
                         editor={editor}
                         id="editor"
                         onChange={() => {
-                            // Saves the document JSON to state.
-                            setBlocks(editor.document);
+                            setBlocks(editor.document); // Saves the document JSON to state.
                         }}
                         className="mb-5"
                     />
