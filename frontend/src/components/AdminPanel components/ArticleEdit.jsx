73f36bbbd6131d8a4b6c1f110bc82a6877b113af
JR-Koders <jules.caoeiros@gmail.com>
2024-08-24 20:09:24 +0200

    Added error screen to ArticleEdit.jsx

--- frontend/src/components/AdminPanel components/ArticleEdit.jsx
+++ frontend/src/components/AdminPanel components/ArticleEdit.jsx
@@ -7,8 +7,10 @@ import { useParams } from 'react-router-dom';
 const ArticleEdit = () => {
     const { title } = useParams();
     const [loading, setLoading] = useState(true);
-    const [articleData, setArticleData] = useState({});
+    const [error, setError] = useState(false);
+    const [articleData, setArticleData] = useState();
     useEffect(() => {
+        console.log(`going to request article`);
         setLoading(true);
         axios.get(`http://localhost:5555/blog/article/${title}`,
             { withCredentials: true })
@@ -21,9 +23,17 @@ const ArticleEdit = () => {
             })
             .catch((error) => {
                 setLoading(false);
+                setError(true);
             })
         console.log(`Article data after axios: ${JSON.stringify(articleData)}`)
     }, []);
+    if (error) {
+        return <div className="flex-grow bg-black p-12 text-white">
+        <div className="max-w-3xl mx-auto bg-gray-800 p-6 rounded-lg shadow-lg text-center">
+            <span className='text-red-400'>There was an error retrieving the article data</span>
+        </div>
+    </div>
+    }
     if (loading || !articleData) {
         return <div className="flex-grow bg-black p-12 text-white">
             <div className="max-w-3xl mx-auto bg-gray-800 p-6 rounded-lg shadow-lg">
