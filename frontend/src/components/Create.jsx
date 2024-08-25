f5063edac1b6781be3373bf51d95c545529a8659
JR-Koders <jules.caoeiros@gmail.com>
2024-08-25 12:48:00 +0200

    changed tab's title for Create.jsx

--- frontend/src/components/Create.jsx
+++ frontend/src/components/Create.jsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import CreateForm from './CreateForm';
 
 
 const Create = () => {
+    useEffect(() => {
+        document.title = `New Post`;
+    }, []);
     return (
         CreateForm(
             {
