d09f76d2eecf4a8da935e5f086b35cdd420c92d2
JR-Koders <jules.caoeiros@gmail.com>
2024-09-02 19:29:14 +0200

    updating cookies attributes for google oauth

--- backend/routes/OAuth.js
+++ backend/routes/OAuth.js
@@ -70,11 +70,15 @@ router.get('/google/callback',
         res.cookie("token", token, {
             withCredentials: true,
             httpOnly: false,
+            domain: 'jules.tf',
+            sameSite: 'None',
         });
         const isAdmin = req.user.isAdmin;
         res.cookie("isAdmin", isAdmin, {
             withCredentials: true,
             httpOnly: false,
+            domain: 'jules.tf',
+            sameSite: 'None',
         });
         // res.redirect(`http://localhost:5173`);
         res.redirect(`https://blog.jules.tf`);
