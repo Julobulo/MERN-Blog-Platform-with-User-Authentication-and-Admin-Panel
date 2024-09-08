46c6311be29d2141b3a46f19808412a44e4bb2d2
JR-Koders <jules.caoeiros@gmail.com>
2024-09-08 15:32:44 +0200

    added domain field with value .jules.tf to oauth cookies

--- backend/routes/OAuth.js
+++ backend/routes/OAuth.js
@@ -70,12 +70,14 @@ router.get('/google/callback',
         res.cookie("token", token, {
             withCredentials: true,
             httpOnly: false,
+            domain: '.jules.tf',
             sameSite: 'None',
         });
         const isAdmin = req.user.isAdmin;
         res.cookie("isAdmin", isAdmin, {
             withCredentials: true,
             httpOnly: false,
+            domain: '.jules.tf',
             sameSite: 'None',
         });
         // res.redirect(`http://localhost:5173`);
