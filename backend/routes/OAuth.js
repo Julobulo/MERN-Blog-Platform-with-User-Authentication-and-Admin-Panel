1786aad45bf2b7eccc5bd6c5a91352396445c04c
JR-Koders <jules.caoeiros@gmail.com>
2024-10-08 21:46:22 +0200

    trying list of domains

--- backend/routes/OAuth.js
+++ backend/routes/OAuth.js
@@ -71,7 +71,7 @@ router.get('/google/callback',
         res.cookie("token", token, {
             withCredentials: true,
             httpOnly: false,
-            domain: 'https://blog.jules.tf',
+            domain: ['jules.tf', 'blog.jules.tf'],
             sameSite: 'None',
             secure: true,
         });
@@ -79,7 +79,7 @@ router.get('/google/callback',
         res.cookie("isAdmin", isAdmin, {
             withCredentials: true,
             httpOnly: false,
-            domain: 'https://blog.jules.tf',
+            domain: ['jules.tf', 'blog.jules.tf'],
             sameSite: 'None',
             secure: true,
         });
